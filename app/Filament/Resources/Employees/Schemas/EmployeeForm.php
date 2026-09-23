1abf18af744b96862706a2b5f1e64f29b8106381
adrnhvisy <adrianhavis47@gmail.com>
2026-09-23 10:36:09 +0700

    tables employee

--- app/Filament/Resources/Employees/Schemas/EmployeeForm.php
+++ app/Filament/Resources/Employees/Schemas/EmployeeForm.php
@@ -4,7 +4,11 @@ namespace App\Filament\Resources\Employees\Schemas;
 
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+use Filament\Tables\Columns\ImageColumn;
+use Illuminate\Image\Image;
 
 class EmployeeForm
 {
@@ -12,29 +16,41 @@ class EmployeeForm
     {
         return $schema
             ->components([
-                TextInput::make('user_id')
-                    ->required()
-                    ->numeric(),
-                TextInput::make('department_id')
-                    ->required()
-                    ->numeric(),
-                TextInput::make('position_id')
-                    ->required()
-                    ->numeric(),
-                TextInput::make('address'),
-                TextInput::make('pob'),
-                DatePicker::make('dob'),
-                TextInput::make('gender'),
-                TextInput::make('religion'),
-                TextInput::make('phone_number')
-                    ->tel(),
-                TextInput::make('salary')
-                    ->required()
-                    ->numeric()
-                    ->default(0),
-                DatePicker::make('start_date'),
-                DatePicker::make('end_date'),
-                TextInput::make('status'),
+                Section::make('User Information')
+                    ->icon('heroicon-o-user')
+                    ->columnSpan(2)
+                    ->schema([
+                        Group::make()
+                            ->relationship('user')
+                            ->schema([
+                                TextInput::make('name')
+                                    ->placeholder('Enter name')
+                                    ->required()
+                                    ->maxLength(255),
+                                TextInput::make('email')
+                                    ->placeholder('Enter email')
+                                    ->required()
+                                    ->email()
+                                    ->maxLength(255),
+                                TextInput::make('password')
+                                    ->placeholder('Enter password')
+                                    ->required()
+                                    ->password()
+                                    ->maxLength(255),  
+                            ])
+                    ]),
+                    
+                    Section::make('Image')
+                    ->icon('heroicon-o-camera')
+                    ->columnSpan(1)
+                    ->schema([
+                        Group::make()
+                        ->relationship('employee')
+                        ->schema([
+                            ImageColumn::make('image')
+                        ])
+                    ])
+                    
             ]);
     }
 }
